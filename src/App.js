81fe28e3c2a25172b656aa3815e0b835bd47db45
Diego Alzate <diegoff@gmail.com>
2020-03-24 10:45:51 -0500

    Text h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
    return (
       <div className="App">
-         <h1>My first react app</h1>
+         <h1>Mi primera aplicación</h1>
          <p>REACT</p>
       </div>
    );
